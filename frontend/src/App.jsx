f14e3517a53586aac7a4c516fe33ee6bc6bee58c
Guillermo Reyes <Guillermo Reyes@orgro.team>
2024-12-07 21:21:42 -0800

    Simple Nav created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,42 @@
 
-import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Link} from 'react-router-dom';
 import './App.css'
 import MaintenanceEntry from './component/MaintenanceEntry';
 
-import MaintLogList from './component/MaintLogList';
 import MaintenanceLogs from './component/MaintenanceLogs';
 function App() {
   
   return (
     <>
-    
       <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<MaintenanceLogs />} />
-                <Route path="/create" element={<MaintenanceEntry />} />
-            </Routes>
+        <nav style={styles.nav}>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <Link to="/create">Create</Link>
+          <div>
+            
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<MaintenanceLogs />} />
+          <Route path="/create" element={<MaintenanceEntry />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
 }
 
-export default App
+const styles= {
+  nav:{
+    height:"20px",
+    padding:"30px 0",
+    width:"100%",
+    
+    display:"flex",
+    gap:"20px",
+    justifyContent:"flex-end",
+    fontSize:"1.2rem"
+  }
+}
+export default App;
